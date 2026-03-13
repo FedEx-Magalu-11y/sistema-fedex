d1cb242690da5d1b7438c4ff253e7557a7015eb4
FedEx-Magalu-11y <iguiimcl@gmail.com>
2026-03-13 18:01:54 -0300

    Delete static/js/supabaseClient.js

--- static/js/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
-
-const supabaseUrl = "https://opiwznepfjcinxrymrxw.supabase.co"
-
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9waXd6bmVwZmpjaW54cnltcnh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4OTQ3MDQsImV4cCI6MjA4ODQ3MDcwNH0.bcfSAqQzPNs1BISoKBUP2p1KU0MIgCzFimX4OzhU0yU"
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
