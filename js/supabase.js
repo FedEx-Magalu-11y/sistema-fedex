9aca73ce50fff0f3dc223bec88be688877448b9a
FedEx-Magalu-11y <iguiimcl@gmail.com>
2026-03-14 16:26:10 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://opiwznepfjcinxrymrxw.supabase.co"
 const SUPABASE_KEY = "sb_publishable_F7z4q7UuxxNc13r4rDubZA_LsFF1fEB"
 
-const supabase = window.supabase.createClient(
+window.db = window.supabase.createClient(
 SUPABASE_URL,
 SUPABASE_KEY
 )
