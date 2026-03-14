b03bc9922a3683e6e6ca8d0ffd7c7607e4922aa4
FedEx-Magalu-11y <iguiimcl@gmail.com>
2026-03-14 16:32:49 -0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,53 +1,68 @@
-async function criar(){
+const params=new URLSearchParams(window.location.search)
 
-const code=document.getElementById("codigo").value
+const code=params.get("code")
 
-const nome=document.getElementById("nome").value
+async function carregar(){
 
-const endereco=document.getElementById("endereco").value
-
-await supabase
+const {data}=await db
 .from("trackings_test")
-.insert({
+.select("*")
+.eq("code",code)
+.single()
 
-code:code,
-recipient_name:nome,
-address:endereco,
-timeline:[{
-status:"Postado",
-location:"Centro Logístico",
-date:new Date()
-}]
+if(!data){
 
-})
+document.body.innerHTML="Código não encontrado"
 
-alert("Criado")
-
-listar()
+return
 
 }
 
-async function listar(){
+document.querySelector(".tracking-code").innerText=data.code
 
-const {data}=await supabase
-.from("trackings_test")
-.select("*")
+document.querySelector(".recipient-name").innerText=data.recipient_name
 
-const lista=document.getElementById("lista")
+document.querySelector(".recipient-address").innerText=data.address
 
-lista.innerHTML=""
+const timeline=document.querySelector(".tracking-timeline")
 
-data.forEach(t=>{
+timeline.innerHTML=""
 
-lista.innerHTML+=`
+data.timeline.forEach((t,i)=>{
 
-<div>
+timeline.innerHTML+=`
 
-${t.code}
+<div class="timeline-item ${i==data.timeline.length-1?"active":""}">
 
-<button onclick="copiar('${t.code}')">Copiar link</button>
+<div class="timeline-dot"></div>
 
-<button onclick="excluir('${t.id}')">Excluir</button>
+<div class="timeline-content">
+
+<div class="timeline-date">
+
+${new Date(t.date).toLocaleString("pt-BR")}
+
+</div>
+
+<div class="timeline-status">
+
+${t.title}
+
+</div>
+
+<div class="timeline-description">
+
+${t.description}
+
+</div>
+
+<div class="timeline-location">
+
+${t.location}
+
+</div>
+
+</div>
 
 </div>
 
@@ -57,25 +72,4 @@ ${t.code}
 
 }
 
-function copiar(code){
-
-navigator.clipboard.writeText(
-window.location.origin+"/rastreamento.html?code="+code
-)
-
-alert("Link copiado")
-
-}
-
-async function excluir(id){
-
-await supabase
-.from("trackings_test")
-.delete()
-.eq("id",id)
-
-listar()
-
-}
-
-listar()
+carregar()
