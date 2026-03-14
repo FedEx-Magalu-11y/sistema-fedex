9cca444e4238c91fcd5ea2827d8e3733993faa10
FedEx-Magalu-11y <iguiimcl@gmail.com>
2026-03-14 14:37:30 -0300

    Update cep.js

--- api/cep.js
+++ api/cep.js
@@ -9,7 +9,7 @@ return res.status(400).json({erro:"CEP não informado"})
 }
 
 const response = await fetch(
-"https://api-core.theretech.com.br/geo/cep?cep=" + cep,
+"https://api-core.theretech.com.br/geo/cep?cep="+cep,
 {
 headers:{
 "X-API-Key":"0d5ac812-78ac-4e06-863a-e7532adb81c8.ROLMFYY7SFI7MUQFCZANY4QJEVMDM7FX"
@@ -17,7 +17,18 @@ headers:{
 }
 )
 
-const data = await response.json()
+const text = await response.text()
+
+let data
+
+try{
+data = JSON.parse(text)
+}catch{
+return res.status(500).json({
+erro:"API retornou texto inválido",
+resposta:text
+})
+}
 
 res.status(200).json(data)
 
