daea0a548df1b5fad0cb35376253695fce835479
FedEx-Magalu-11y <iguiimcl@gmail.com>
2026-03-14 14:35:09 -0300

    Update cep.js

--- api/cep.js
+++ api/cep.js
@@ -9,7 +9,7 @@ return res.status(400).json({erro:"CEP não informado"})
 }
 
 const response = await fetch(
-"https://api-core.theretech.com.br/geo/cep/" + cep,
+"https://api-core.theretech.com.br/geo/cep?cep=" + cep,
 {
 headers:{
 "X-API-Key":"0d5ac812-78ac-4e06-863a-e7532adb81c8.ROLMFYY7SFI7MUQFCZANY4QJEVMDM7FX"
@@ -17,25 +17,13 @@ headers:{
 }
 )
 
-const text = await response.text()
+const data = await response.json()
 
-try{
-
-const json = JSON.parse(text)
-return res.status(200).json(json)
-
-}catch{
-
-return res.status(500).json({
-erro:"Resposta inválida da API Retech",
-resposta:text
-})
-
-}
+res.status(200).json(data)
 
 }catch(error){
 
-return res.status(500).json({
+res.status(500).json({
 erro:"Erro interno",
 detalhe:error.message
 })
